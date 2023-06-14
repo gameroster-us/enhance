dee6fd4d58cb9a637f0060d93b4d8cacdf919c00
gameroster <gameroster.us@gmail.com>
2023-06-14 12:45:05 +0530

    Remove routes for resister user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
-  devise_for :users
   
   resources :employees
   resources :jobs
   resources :assessments
+
+  devise_for :users, :skip => :registration
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
