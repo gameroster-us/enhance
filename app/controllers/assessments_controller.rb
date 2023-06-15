b8b0de7f0dd0bd728df92912d196a046e80d91f1
gameroster <gameroster.us@gmail.com>
2023-06-15 07:55:59 +0530

    Modify the code

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -13,15 +13,15 @@ class AssessmentsController < ApplicationController
   def create
     @employee = params[:options]
     Job.all.each do |job|
-      @job = Hash[job.employees.map {|e| [e.id, Assessment.where(job_id: job.id, employee_id: e.id).count]}]
-      @job = @job.sort_by {|_key, value| value }
-      @job = @job.reject { |a| @employee.include?(a.to_s) }
-      index = 0
+      @employees = Hash[job.employees.map {|e| [e.id, Assessment.where(job_id: job.id, employee_id: e.id).count]}]
+      @employees = @employees.sort_by {|_key, value| value }
+      @employees.select! { |emp| @employee.include?(emp[0].to_s) }
+
       job.number_of_capacity.times do
-        if @job[index].present?
-          emp_id = @job[index][0]
-          Assessment.create(job_id: job.id, employee_id: emp_id)
-          index += 1
+        if @employees[0].present?
+          emp_id = @employees[0][0]
+          @ass = Assessment.create(job_id: job.id, employee_id: emp_id)
+          @employee = @employee.reject { |val| val == emp_id.to_s} if @ass.present?
         end
       end
     end
