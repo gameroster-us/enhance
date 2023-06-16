66a3c2e6290ec8d3e252c35df247e008020692ff
gameroster <gameroster.us@gmail.com>
2023-06-16 11:48:56 +0530

    Set default value in filter

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -12,19 +12,39 @@ class AssessmentsController < ApplicationController
 
   def create
     @employee = params[:options]
-    Job.all.each do |job|
-      @employees = Hash[job.employees.map {|e| [e.id, Assessment.where(job_id: job.id, employee_id: e.id).count]}]
-      @employees = @employees.sort_by {|_key, value| value }
-      @employees.select! { |emp| @employee.include?(emp[0].to_s) }
+    @job = []
 
-      job.number_of_capacity.times do
-        if @employees[0].present?
-          emp_id = @employees[0][0]
-          @ass = Assessment.create(job_id: job.id, employee_id: emp_id)
+    Job.count.times do |j|
+      loop do
+        @find_job = find_job
+        if !@job.include?(@find_job)
+          @job << @find_job
+          break
+        end
+      end
+
+      @employees = Hash[@find_job.employees.map {|e| [e.id, Assessment.where(job_id: @find_job.id, employee_id: e.id).count]}]
+      @employees = @employees.sort_by {|_key, value| value }
+
+      @find_job.number_of_capacity.times do
+        @employees.select! { |emp| @employee.include?(emp[0].to_s) }
+
+        if @employees.first.present?
+          emp_id = @employees.first[0]
+          @ass = Assessment.create(job_id: @find_job.id, employee_id: emp_id)
           @employee = @employee.reject { |val| val == emp_id.to_s} if @ass.present?
         end
       end
     end
     redirect_to assessments_path
+  end
+
+  private
+  def find_job
+    if Assessment.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).blank?
+      Job.second
+    else
+      Job.all.sample
+    end
   end
 end
